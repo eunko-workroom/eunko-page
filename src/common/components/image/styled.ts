1a2cd804bea55edc3e051730c0c858c5e60b2f29
Yuno <yunho101775@gmail.com>
2020-06-24 00:38:26 +0900

    fix image height

--- src/common/components/image/styled.ts
+++ src/common/components/image/styled.ts
@@ -13,7 +13,6 @@ export const Wrapper = styled.div`
 export const Image = styled.img`
   object-fit: cover;
   width: 100%;
-  height: 100px;
   animation-name: ${FadeIn};
   animation-duration: 3s;
   animation-iteration-count: 1;
