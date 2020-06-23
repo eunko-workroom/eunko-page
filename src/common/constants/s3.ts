51ec44a1a13b121c5dce0d6312fcaa864d5080c7
eunko <eunko.workroom@gmail.com>
2020-06-24 00:14:46 +0900

    Fix aws key

--- src/common/constants/s3.ts
+++ src/common/constants/s3.ts
@@ -1,6 +1,6 @@
 export const bucketName = "eunko.workroom";
 export const bucketRegion = "ap-northeast-2";
 export const apiVersion = "2006-03-01";
-export const accessKeyId = "AKIAT4K7DKVD6NFIHTOT";
-export const secretAccessKey = "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";
+export const accessKeyId = "AKIAT4K7DKVD4XCAD7WF";
+export const secretAccessKey = "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";
 export const bucketUrl = `https://s3.ap-northeast-2.amazonaws.com/${bucketName}/`;
